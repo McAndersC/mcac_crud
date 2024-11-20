291b7e938a451c79549656d922e447ca2f376bca
Anders Christensen <anders@medieskolerne.dk>
2024-11-20 12:22:37 +0100

    klient form

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,1 +1,12 @@
 console.log('Hello Client')
+
+
+const onFormSubmit = (e) => {
+
+    e.preventDefault();
+    console.log('FORM', e.target)
+
+}
+
+let form = document.querySelector('#userForm');
+form.addEventListener('submit', onFormSubmit)
